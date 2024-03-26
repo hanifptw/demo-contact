585209336d0e0159a6e11ce67cb19b2d74d1a257
Brilliant Hanif Almubarak <brillianthanif@gmail.com>
2024-03-26 19:48:05 +0700

    feat: add insert contact

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 const listContact = [
   {
+    id: 1,
     fullName: "Hanif",
     age: 25,
     hobby: "Design",
   },
   {
+    id: 2,
     fullName: "Brilliant",
     age: 20,
     hobby: "Organization",
   },
   {
+    id: 3,
     fullName: "Almubarak",
     age: 17,
     hobby: "moutainering",
-  }
+  },
 ];
 
+const addContact = (fullName, age, hobby) => {
+  let id = listContact.length + 1;
+
+  listContact.push({ id, fullName, age, hobby });
+};
+
+const renderContact = (item) => {
+  console.log(
+    `${item.id}. Nama Lengkap :${item.fullName} Umur : ${item.age} Hobby : ${item.hobby}`
+  );
+};
 
 
-console.log(listContact);
+addContact("brilliant hanif", 25, "memancing");
+listContact.map(renderContact);
