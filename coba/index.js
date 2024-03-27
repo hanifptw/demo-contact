ac6487e1c3fd3be3eae4f1e4360ef80d20c2c469
Brilliant Hanif Almubarak <brillianthanif@gmail.com>
2024-03-27 09:08:12 +0700

    feat: add search, delete and DOM

---
+++ coba/index.js
@@ -0,0 +1,103 @@
+let contacts = [
+    {
+      id: 1,
+      fullName: "Brilliant",
+      phone: "+083321232211",
+      email: "brilliant@mail.com",
+      hobby: "Memancing",
+      age: 30,
+    },
+    {
+      id: 2,
+      fullName: "Hanif",
+      phone: "+082134843810",
+      email: "hanif@hanif.com",
+      hobby: "Memasak",
+      age: 52,
+    },
+    {
+      id: 3,
+      fullName: "Jeff Bezos",
+      phone: "+0824567890",
+      email: "jeff@jeff.com",
+      hobby: "Mengkoding",
+      age: 60,
+    },
+  ];
+  
+  const searchInputElement = document.getElementById("search-input");
+  const addContactFormElement = document.getElementById("add-contact-form");
+  const contactsContainerElement = document.getElementById("contacts-container");
+  
+  function renderContacts() {
+    const queryString = window.location.search;
+    const params = new URLSearchParams(queryString);
+    const keyword = params.get("q");
+  
+    if (keyword) {
+      searchInputElement.value = keyword;
+  
+      const filteredContacts = contacts.filter((contact) =>
+        contact.fullName.toLowerCase().includes(keyword.toLowerCase())
+      );
+  
+      contacts = filteredContacts;
+    }
+  
+    const contactItemElements = contacts.map(
+      (contact) => `<li>
+    <h2>${contact.fullName}</h2>
+    <p>${contact.email}</p>
+    <p>${contact.phone}</p>
+    <p>${contact.hobby}</p>
+    <p>${contact.age}</p>
+
+    
+    <div>
+      <button onclick="deleteContactById(${contact.id})">Delete</button>
+    </div>
+
+
+  </li>
+  `
+    );
+    const contactItems = contactItemElements.join("");
+    contactsContainerElement.innerHTML = contactItems;
+  }
+  
+  function addContact(event) {
+    event.preventDefault();
+    const contactFormData = new FormData(addContactFormElement);
+  
+    const newContact = {
+      id: contacts[contacts.length - 1].id + 1,
+      fullName: contactFormData.get("fullName"),
+      email: contactFormData.get("email"),
+      phone: contactFormData.get("phone"),
+      hobby: contactFormData.get("hobby"),
+      birthDate: new Date(contactFormData.get("birthDate")),
+      age:
+      new Date().getFullYear() -
+      new Date(contactFormData.get("birthDate")).getFullYear(),
+    };
+  
+    contacts.push(newContact);
+    renderContacts();
+  }
+  
+  function deleteContactById(id) {
+    const updatedContacts = contacts.filter(
+      (contact) => contact.id !== Number(id)
+    );
+  
+    contacts = updatedContacts;
+    renderContacts();
+  }
+  
+  function updateContactById(id) {
+    // delete
+  }
+  
+  window.addEventListener("load", renderContacts);
+  
+  addContactFormElement.addEventListener("submit", addContact);
--- index.js
+++ index.js
@@ -1,35 +1,66 @@
-const listContact = [
+const contacts = [
   {
     id: 1,
     fullName: "Hanif",
+    email: "Hanif@gmail.com",
     age: 25,
     hobby: "Design",
   },
   {
     id: 2,
     fullName: "Brilliant",
+    email: "Brilliant@gmail.com",
     age: 20,
     hobby: "Organization",
   },
   {
     id: 3,
     fullName: "Almubarak",
+    email: "Hanif@gmail.com",
     age: 17,
     hobby: "moutainering",
   },
 ];
 
-const addContact = (fullName, age, hobby) => {
-  let id = listContact.length + 1;
-  listContact.push({ id, fullName, age, hobby });
-  listContact.map(renderContact);
-};
+const contactsListContainerElement = document.getElementById("contact-list");
+const addContactFormElement = document.getElementById("add-contact-form");
 
-const renderContact = (item) => {
-  console.log(
-    `${item.id}. Nama Lengkap :${item.fullName} Umur : ${item.age} Hobby : ${item.hobby}`
+function addContact() {
+  const contactFormData = new FormData(addContactFormElement);
+
+  const newContact = {
+    id: contacts[contacts.length - 1].id + 1,
+    fullName: contactFormData.get("input-fullName"),
+    email: contactFormData.get("input-email"),
+    hobby: contactFormData.get("input-hobby"),
+    // age: contactFormData.get("input-birthDate"),
+    age:
+      new Date().getFullYear() -
+      new Date(contactFormData.get("input-birthDate")).getFullYear(),
+  };
+
+  contacts.push(newContact);
+  renderContacts();
+}
+
+
+const renderContacts = () => {
+  const contactItemElements = contacts.map(
+    (contact) => `<li>
+<h3>${contact.fullName}</h3>
+<p>${contact.email}</p>
+<p>${contact.age}</p>
+<p>${contact.hobby}</p>
+<div>
+  <button onclick="deleteContactById(${contact.id})">Delete</button>
+</div>
+</li>
+`
   );
-};
 
-addContact("brilliant hanif", 25, "memancing");
-listContact.map(renderContact);
+  const contactItems = contactItemElements.join("");
+  contactsListContainerElement.innerHTML = contactItems;
+};
+renderContacts();
+addContactFormElement.addEventListener("submit", addContact);
+
